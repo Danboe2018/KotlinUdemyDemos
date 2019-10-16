9e3d5f261275e1ab9bfcfd3e858ae0441120170e
Dan Boehmke <Daniel@webappclouds.com>
2019-10-16 17:21:16 -0400

    Nested if statement

--- src/IFELSEIF.kt
+++ src/IFELSEIF.kt
@@ -1,17 +1,9 @@
-
-
-
 fun main(){
-
-
     print("Enter your grade: ")
     val grade = readLine()!!.toInt()
 
-
     if(grade>= 90){
-
         println("A")
-
     } else if(grade in 80..89){
         println("B")
     }else if(grade in 70..79){
@@ -20,8 +12,5 @@ fun main(){
         println("Fail")
     }
 
-
     println("End of App")
-
-
 }
