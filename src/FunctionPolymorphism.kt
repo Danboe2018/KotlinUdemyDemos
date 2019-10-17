7cf209aea73fd4c2c5b12d20a89aacaa7c794d60
Dan Boehmke <Daniel@webappclouds.com>
2019-10-17 15:04:36 -0400

    PolyMorphism

---
+++ src/FunctionPolymorphism.kt
@@ -0,0 +1,14 @@
+fun showInfo(cardID:Int){
+    // check if card is valid
+    println("CardID: $cardID")
+}
+
+fun  showInfo(name:String){
+    // check if user is active
+    println("Name: $name")
+}
+
+fun  main(){
+    showInfo("Hussein Alrubaye")
+    showInfo(7264374)
+}
