723882e427e4dbc8376ff8b0cd89974757845694
Dan Boehmke <Daniel@webappclouds.com>
2019-10-17 15:20:44 -0400

    Scope

---
+++ src/Scope.kt
@@ -0,0 +1,13 @@
+var name:String?=null
+
+fun showUserInfo(){
+    name="Welcome $name"
+    println(" $name")
+}
+
+fun main() {
+    name = "Hussein"
+    println(" $name")
+    showUserInfo()
+    println(" $name")
+}
